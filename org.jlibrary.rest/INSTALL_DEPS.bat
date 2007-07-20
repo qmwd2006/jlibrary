4940446b139e77453bee559adb9be43ce8968328
perez_martin <perez_martin@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-07-20 19:02:36 +0000

    Not needed any more. Dependencies moved to local repo.

--- org.jlibrary.rest/INSTALL_DEPS.bat
+++
@@ -1,4 +0,0 @@
-call mvn install:install-file -DgroupId=org.restlet -DartifactId=restlet -Dversion=1.0.1 -Dpackaging=jar -DgeneratePom=true -Dfile=deps/org.restlet-1.0.1.jar
-call mvn install:install-file -DgroupId=org.restlet -DartifactId=restlet-spring -Dversion=1.0.1 -Dpackaging=jar -DgeneratePom=true -Dfile=deps/org.restlet.ext.spring_2.0-1.0.1.jar
-call mvn install:install-file -DgroupId=com.noelios -DartifactId=noelios -Dversion=1.0.1 -Dpackaging=jar -DgeneratePom=true -Dfile=deps/com.noelios.restlet-1.0.1.jar
-call mvn install:install-file -DgroupId=com.noelios -DartifactId=noelios-servlet -Dversion=1.0.1 -Dpackaging=jar -DgeneratePom=true -Dfile=deps/com.noelios.restlet.ext.servlet_2.4-1.0.1.jar
