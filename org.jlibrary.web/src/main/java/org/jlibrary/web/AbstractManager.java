2b612740a251998743b83b151efdd0752293d033
danilat <danilat@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-07-30 19:10:21 +0000

    cambio en el acceso a los ticket

--- org.jlibrary.web/src/main/java/org/jlibrary/web/AbstractManager.java
+++ org.jlibrary.web/src/main/java/org/jlibrary/web/AbstractManager.java
@@ -1,19 +1,27 @@
 package org.jlibrary.web;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.jlibrary.core.entities.Repository;
 import org.jlibrary.core.entities.Ticket;
 import org.jlibrary.core.repository.exception.RepositoryException;
 import org.jlibrary.core.repository.exception.RepositoryNotFoundException;
 import org.jlibrary.core.security.SecurityException;
 import org.jlibrary.web.conf.JLibraryConfiguration;
+import org.jlibrary.web.services.TicketService;
 
 public abstract class AbstractManager {
+	protected static final TicketService ticketService=TicketService.getTicketService();
 	protected static final JLibraryConfiguration jlibrary=JLibraryConfiguration.newInstance();
 	public Ticket getTicket() {
-		return jlibrary.getTicket();
+		HttpServletRequest request=(HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		return ticketService.getTicket(request,jlibrary.getRepositoryName());
 	}
-	public void setTicket(Ticket ticket) {
-		jlibrary.setTicket(ticket);
+	
+	public void setTicket(Ticket ticket){
+		HttpServletRequest request=(HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		request.getSession(true).setAttribute(TicketService.SESSION_TICKET_ID,ticket);
 	}
 	
 	public Repository getRepository() throws RepositoryNotFoundException, RepositoryException, SecurityException {
