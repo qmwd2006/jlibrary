7026904432a670a0bead691cb11fc85771d7d358
perez_martin <perez_martin@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-08-05 19:54:22 +0000

    Added initial search functionality

---
+++ org.jlibrary.web/src/main/java/org/jlibrary/web/freemarker/SearchTemplateProcessor.java
@@ -0,0 +1,88 @@
+/*
+* jLibrary, Open Source Document Management System
+* 
+* Copyright (c) 2003-2006, Martín Pérez Mariñán, and individual 
+* contributors as indicated by the @authors tag. See copyright.txt in the
+* distribution for a full listing of individual contributors.
+* All rights reserved.
+* 
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the Modified BSD License as published by the Free 
+* Software Foundation.
+* 
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Modified
+* BSD License for more details.
+* 
+* You should have received a copy of the Modified BSD License along with 
+* this software; if not, write to the Free Software Foundation, Inc., 
+* 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the
+* FSF site: http://www.fsf.org.
+*/
+package org.jlibrary.web.freemarker;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.jlibrary.core.search.SearchResultSet;
+import org.jlibrary.web.content.WordCounter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author martin
+ *
+ * Template processor for search results
+ */
+public class SearchTemplateProcessor implements FreemarkerTemplateProcessor {
+
+	static Logger logger = LoggerFactory.getLogger(SearchTemplateProcessor.class);
+	
+	private Collection results;
+	private RepositoryContext context;
+	private FreemarkerExporter exporter;
+
+	/**
+	 * Document template processor
+	 * 
+	 * @param exporter Main freemarker template exporter
+	 * @param resultSet search results
+	 * @param context Context repository for this processor
+	 */
+	public SearchTemplateProcessor(FreemarkerExporter exporter,
+								   Collection results, 
+								   RepositoryContext context) {
+
+		this.results = results;
+		this.context = context;
+		this.exporter = exporter;
+	}
+	
+	public String processTemplate(FreemarkerFactory factory) throws ExportException {
+
+		return processTemplate(factory,factory.getPage("search.ftl"));
+	}	
+	
+
+	public String processTemplate(FreemarkerFactory factory,
+								  Page page) throws ExportException {
+		
+		page.expose(FreemarkerVariables.REPOSITORY,context.getRepository());
+		page.expose(FreemarkerVariables.SEARCH_RESULTS, results);		
+		page.expose(FreemarkerVariables.TICKET, context.getTicket());
+		
+		page.expose(FreemarkerVariables.DATE, new Date());
+
+		String rootURL = exporter.getRootURL(context.getRepository().getRoot());
+		String repositoryURL = exporter.getRepositoryURL();
+		
+		page.expose(FreemarkerVariables.ROOT_URL,rootURL);
+		page.expose(FreemarkerVariables.REPOSITORY_URL,repositoryURL);
+		page.expose(FreemarkerVariables.CATEGORIES_ROOT_URL,repositoryURL+"/categories");
+		page.expose(FreemarkerVariables.LOCATION_URL, "");
+			
+		page.expose(FreemarkerVariables.PAGE_KEYWORDS,"jlibrary, search, results");
+		return page.getAsString();
+	}
+}
---
+++ org.jlibrary.web/src/main/java/org/jlibrary/web/servlet/JLibrarySearchServlet.java
@@ -0,0 +1,132 @@
+package org.jlibrary.web.servlet;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.jlibrary.core.entities.Repository;
+import org.jlibrary.core.entities.ServerProfile;
+import org.jlibrary.core.entities.Ticket;
+import org.jlibrary.core.factory.JLibraryServiceFactory;
+import org.jlibrary.core.profiles.LocalServerProfile;
+import org.jlibrary.core.repository.RepositoryService;
+import org.jlibrary.core.search.SearchService;
+import org.jlibrary.web.freemarker.FreemarkerExporter;
+import org.jlibrary.web.freemarker.RepositoryContext;
+import org.jlibrary.web.services.TicketService;
+
+@SuppressWarnings("serial")
+public class JLibrarySearchServlet extends HttpServlet {
+
+	private static Logger logger = Logger.getLogger(JLibrarySearchServlet.class);
+
+	private ServerProfile profile = new LocalServerProfile();
+
+	private String repositoryName;
+	
+	@Override
+	public void init() throws ServletException {
+
+		super.init();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		processContent(req,resp);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		processContent(req,resp);
+	}
+
+	private void processContent(HttpServletRequest req, HttpServletResponse resp) {
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("Received search request");
+		}
+		repositoryName = req.getParameter("repository");
+		String text = req.getParameter("text");
+		if ((repositoryName == null) || (text == null)) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Invalid request: repositoryName=" + repositoryName + ", text=" + text);
+			}
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		
+		String type = req.getParameter("type");
+		if (type == null) {
+			type = SearchService.SEARCH_CONTENT;
+		}
+		
+		Ticket ticket = TicketService.getTicketService().getTicket(req, repositoryName);
+		SearchService searchService = 
+			JLibraryServiceFactory.getInstance(profile).getSearchService();
+		RepositoryService repositoryService = 
+			JLibraryServiceFactory.getInstance(profile).getRepositoryService();
+		try {
+			
+			Repository repository = 
+				repositoryService.findRepository(repositoryName, ticket);
+			repository.setServerProfile(new LocalServerProfile());
+			repository.setTicket(ticket);
+			
+			Collection results = searchService.search(ticket, text, type);
+			String output = exportResults(req,ticket,repository,results);
+			resp.getOutputStream().write(output.getBytes());
+			resp.flushBuffer();
+		} catch (Exception e) {
+			logger.error(e.getMessage(),e);
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		
+		return;
+	}
+	private String exportResults(HttpServletRequest request, 
+								  Ticket ticket, 
+								  Repository repository, 
+								  Collection results) {
+		
+		try {
+			String templatesDirectory = 
+				"/software/apache-tomcat-6.0.13-pruebas/webapps/jlibrary/templates/andreas";
+			RepositoryContext context = 
+				new RepositoryContext(repository,templatesDirectory,null);
+			context.setTicket(ticket);
+			FreemarkerExporter exporter = new FreemarkerExporter();
+			
+			exporter.setRootURL(getRootURL(request));
+			exporter.setRepositoryURL(getRepositoryURL(request));
+			
+			exporter.initExportProcess(context);
+			return exporter.exportSearchResults(results, context);
+		} catch (Exception e) {
+			logger.error(e.getMessage(),e);
+			return "";
+		}
+	}
+	
+	private String getRootURL(HttpServletRequest request) {
+
+		return request.getScheme( ) + "://"
+				+ request.getLocalAddr( )  + ":"
+				+ request.getLocalPort( )
+				+ request.getContextPath( );
+	}
+
+	private String getRepositoryURL(HttpServletRequest request) {
+		
+		return getRootURL(request) + "/repositories/" + repositoryName;
+	}
+}
