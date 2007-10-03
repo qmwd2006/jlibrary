b96f3aea1a97107e7799bd1eaaeab1541c36aca7
danilat <danilat@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-10-03 23:44:35 +0000

    first spring integration

---
+++ org.jlibrary.web/src/main/java/org/jlibrary/web/services/TemplateService.java
@@ -0,0 +1,13 @@
+package org.jlibrary.web.services;
+
+public class TemplateService {
+	private String templateDirectory;
+
+	public String getTemplateDirectory() {
+		return templateDirectory;
+	}
+
+	public void setTemplateDirectory(String templateDirectory) {
+		this.templateDirectory = templateDirectory;
+	}
+}
