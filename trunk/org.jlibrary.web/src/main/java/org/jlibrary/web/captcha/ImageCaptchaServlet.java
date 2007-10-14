a81471a316933a65663ddf3ec0853c6e923c6913
perez_martin <perez_martin@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-10-14 10:04:13 +0000

    Added a captcha to the register page.

---
+++ trunk/org.jlibrary.web/src/main/java/org/jlibrary/web/captcha/ImageCaptchaServlet.java
@@ -0,0 +1,87 @@
+/*
+* jLibrary, Open Source Document Management System
+* 
+* Copyright (c) 2003-2006, Martín Pérez Mariñán, and individual 
+* contributors as indicated by the @authors tag. See copyright.txt in the
+* distribution for a full listing of individual contributors.
+* All rights reserved.
+* 
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the Modified BSD License as published by the Free 
+* Software Foundation.
+* 
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Modified
+* BSD License for more details.
+* 
+* You should have received a copy of the Modified BSD License along with 
+* this software; if not, write to the Free Software Foundation, Inc., 
+* 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the
+* FSF site: http://www.fsf.org.
+*/
+package org.jlibrary.web.captcha;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.octo.captcha.service.CaptchaServiceException;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
+public class ImageCaptchaServlet extends HttpServlet {
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+        super.init(servletConfig);
+    }
+
+
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        
+       byte[] captchaChallengeAsJpeg = null;
+       // the output stream to render the captcha image as jpeg into
+        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+        try {
+        // get the session id that will identify the generated captcha. 
+        //the same id must be used to validate the response, the session id is a good candidate!
+        String captchaId = httpServletRequest.getSession().getId();
+        // call the ImageCaptchaService getChallenge method
+            BufferedImage challenge =
+                    CaptchaService.getInstance().getImageChallengeForID(captchaId,
+                            httpServletRequest.getLocale());
+            
+            // a jpeg encoder
+            JPEGImageEncoder jpegEncoder =
+                    JPEGCodec.createJPEGEncoder(jpegOutputStream);
+            jpegEncoder.encode(challenge);
+        } catch (IllegalArgumentException e) {
+            httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        } catch (CaptchaServiceException e) {
+            httpServletResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+
+        // flush it in the response
+        httpServletResponse.setHeader("Cache-Control", "no-store");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+        httpServletResponse.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream =
+                httpServletResponse.getOutputStream();
+        responseOutputStream.write(captchaChallengeAsJpeg);
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+}
