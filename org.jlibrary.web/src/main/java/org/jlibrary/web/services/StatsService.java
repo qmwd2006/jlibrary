1a265a0065b91e65abf34b6e244a8862638aa9bc
danilat <danilat@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-10-19 16:53:49 +0000

    Added MBean and StatsService

---
+++ org.jlibrary.web/src/main/java/org/jlibrary/web/services/StatsService.java
@@ -0,0 +1,58 @@
+package org.jlibrary.web.services;
+
+
+/**
+ * 
+ * @author dlatorre
+ *
+ */
+public class StatsService {
+	private int loggedUsers;
+	private int totalUsers;
+	private static StatsService instance;
+	
+	private StatsService(){
+		loggedUsers=0;
+		totalUsers=0;
+	}
+	
+	public static StatsService newInstance(){
+		if (instance == null) {
+			synchronized(StatsService.class) {
+				if (instance == null)
+					instance= new StatsService();
+				}
+	     	}
+	    return instance;
+	}
+
+	public int getAnonymousUsers() {
+		return totalUsers-loggedUsers;
+	}
+
+	public int getLoggedUsers() {
+		return loggedUsers;
+	}
+
+	public void incLoggedUsers() {
+		this.loggedUsers++;
+	}
+	
+	public void decLoggedUsers() {
+		if(this.loggedUsers>0)
+			this.loggedUsers--;
+	}
+
+	public int getTotalUsers() {
+		return totalUsers;
+	}
+
+	public void incTotalUsers() {
+		this.totalUsers++;
+	}
+	
+	public void decTotalUsers() {
+		if(this.totalUsers>0)
+			this.totalUsers--;
+	}
+}
