b25b8753a552dc0db5cd932ad56d4eed2c2a4cc3
perez_martin <perez_martin@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-10-23 10:50:27 +0000

    Default servlet classes have been refactored out from org.jlibrary.war to this project.

---
+++ trunk/org.jlibrary.server.web/src/main/java/org/jlibrary/servlet/JLibraryStartupContextListener.java
@@ -0,0 +1,72 @@
+package org.jlibrary.servlet;
+
+import java.net.ConnectException;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+import javax.servlet.*;
+
+
+import org.jlibrary.core.entities.Credentials;
+import org.jlibrary.core.entities.ServerProfile;
+import org.jlibrary.core.entities.Ticket;
+import org.jlibrary.core.entities.User;
+import org.jlibrary.core.factory.JLibraryServiceFactory;
+import org.jlibrary.core.factory.ServicesFactory;
+import org.jlibrary.core.profiles.LocalServerProfile;
+import org.jlibrary.core.profiles.RemoteServerProfile;
+import org.jlibrary.core.properties.CategoryProperties;
+import org.jlibrary.core.properties.InvalidPropertyTypeException;
+import org.jlibrary.core.properties.PropertyNotFoundException;
+import org.jlibrary.core.repository.RepositoryService;
+import org.jlibrary.core.repository.exception.CategoryAlreadyExistsException;
+import org.jlibrary.core.repository.exception.RepositoryAlreadyExistsException;
+import org.jlibrary.core.repository.exception.RepositoryException;
+import org.jlibrary.core.repository.exception.RepositoryNotFoundException;
+import org.jlibrary.core.security.SecurityException;
+import org.jlibrary.core.security.SecurityService;
+import org.jlibrary.core.security.exception.AuthenticationException;
+import org.jlibrary.core.security.exception.UserNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JLibraryStartupContextListener implements ServletContextListener {
+	
+	static Logger logger = LoggerFactory.getLogger(JLibraryStartupContextListener.class);
+
+
+	
+	public void contextDestroyed(ServletContextEvent arg0) {
+		logger.info("Stoping jlibrary...");
+		//TODO stop jlibrary server here
+		logger.info("JLibrary stopped!.");
+	}
+
+	public void contextInitialized(ServletContextEvent sce) {
+		logger.info("Configuring security environment...");
+		configureEnvironment(sce.getServletContext());
+		//logger.info("Configuring jlibrary connection...");
+		//TODO start jlibrary server here
+		logger.info("Configuration done!.");
+	}
+
+      private void configureEnvironment(ServletContext sc){
+		try{
+			//JLIBRARY_HOME = sc.getInitParameter("jlibrary.home");
+			Properties env = new Properties();
+			Enumeration names = sc.getInitParameterNames();
+			while (names.hasMoreElements()) {
+	    			String name = (String) names.nextElement();
+	    			if (name.startsWith("java.naming.")) {
+	    				env.put(name, sc.getInitParameter(name));
+	    			}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+}
---
+++ trunk/org.jlibrary.server.web/src/main/java/org/jlibrary/servlet/JLibraryStartupServlet.java
@@ -0,0 +1,104 @@
+/*
+* jLibrary, Open Source Document Management System
+* 
+* Copyright (c) 2003-2006, Martín Pérez Mariñán, and individual 
+* contributors as indicated by the @authors tag. See copyright.txt in the
+* distribution for a full listing of individual contributors.
+* All rights reserved.
+* 
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the Modified BSD License as published by the Free 
+* Software Foundation.
+* 
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Modified
+* BSD License for more details.
+* 
+* You should have received a copy of the Modified BSD License along with 
+* this software; if not, write to the Free Software Foundation, Inc., 
+* 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the
+* FSF site: http://www.fsf.org.
+*/
+package org.jlibrary.servlet;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This servlet is started at jlibrary server startup time
+ * 
+ * @author martin
+ * @deprecated use JLibraryStartupContextListener instead
+ *
+ */ 
+public class JLibraryStartupServlet extends HttpServlet {
+	 
+	private static final long serialVersionUID = 2958184797904116139L;
+	static Logger logger = LoggerFactory.getLogger(JLibraryStartupServlet.class);
+	
+	private static String JLIBRARY_HOME = null;
+		
+	public static final String getJLibraryHome() {
+		
+		return JLIBRARY_HOME;
+	}
+	
+	/** 
+     * Called by the servlet container to indicate to a servlet that 
+     * the servlet is being placed into service. 
+     *       * @param javax.servlet.ServletConfig config 
+     * @throws javax.servlet.ServletException ServletException 
+     */ 
+    public void init(ServletConfig config) throws ServletException { 
+
+    	super.init(config);
+    	
+    	JLIBRARY_HOME = config.getInitParameter("jlibrary.home");
+    	
+    	Properties env = new Properties();
+    	Enumeration names = getServletConfig().getInitParameterNames();
+    	while (names.hasMoreElements()) {
+    	    String name = (String) names.nextElement();
+    	    if (name.startsWith("java.naming.")) {
+    	    	env.put(name, getServletConfig().getInitParameter(name));
+    	    }
+    	}
+    	/*
+        try {
+			JCRSecurityService.initRepository(new InitialContext(env));
+		} catch (NamingException e) {
+
+			logger.error(e.getMessage(),e);
+		}
+		*/
+    } 
+    
+    
+    /** 
+     * Returns information about the servlet, such as author, version, and copyright. By 
+     * default, this method returns an empty string. Override this method to have it return 
+     * a meaningful value. 
+     *  
+     * @return  
+     */ 
+    public java.lang.String getServletInfo() { 
+        
+    	return "jLibrary Startup servlet"; 
+    } 
+        
+    /**
+     * destroy the servlet
+     */
+    public void destroy() {
+
+    	super.destroy();
+    }           
+}
