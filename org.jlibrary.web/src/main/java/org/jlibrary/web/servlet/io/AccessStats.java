cee08e69cde4b862def87c6066c044c121c52deb
perez_martin <perez_martin@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-10-25 09:25:57 +0000

    Bandwidth will be tracked per IP. It is not possible to track it per session because the desktop app creates a new connection per http remote access.

---
+++ org.jlibrary.web/src/main/java/org/jlibrary/web/servlet/io/AccessStats.java
@@ -0,0 +1,62 @@
+/*
+* jLibrary, Open Source Document Management System
+* 
+* Copyright (c) 2003-2006, Martín Pérez Mariñán, and individual 
+* contributors as indicated by the @authors tag. See copyright.txt in the
+* distribution for a full listing of individual contributors.
+* All rights reserved.
+* 
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the Modified BSD License as published by the Free 
+* Software Foundation.
+* 
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Modified
+* BSD License for more details.
+* 
+* You should have received a copy of the Modified BSD License along with 
+* this software; if not, write to the Free Software Foundation, Inc., 
+* 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the
+* FSF site: http://www.fsf.org.
+*/
+package org.jlibrary.web.servlet.io;
+
+/**
+ * This class will hold some access stats per user IP
+ * 
+ * @author mperez
+ *
+ */
+public class AccessStats {
+
+	private Long inputBandwidthUsed;
+	private Long creationTime;
+
+	/**
+	 * Returns the bandwidth used by this session
+	 * 
+	 * @return Long bandwidth
+	 */
+	public Long getInputBandwidthUsed() {
+		
+		return inputBandwidthUsed;
+	}
+
+	public void setInputBandwidthUsed(Long inputBandwidthUsed) {
+		this.inputBandwidthUsed = inputBandwidthUsed;
+	}
+
+	/**
+	 * Returns the time in which this stats object was created
+	 * 
+	 * @return Long time
+	 */
+	public Long getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(Long creationTime) {
+		this.creationTime = creationTime;
+	}
+}
