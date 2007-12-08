63ecabc3fe65b71ee5bc60458d63920fcebd96aa
perez_martin <perez_martin@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2007-12-08 11:28:30 +0000

    Updated session listener name.

---
+++ trunk/org.jlibrary.web/src/main/java/org/jlibrary/web/servlet/listener/JLibrarySessionListener.java
@@ -0,0 +1,81 @@
+/*
+* jLibrary, Open Source Document Management System
+* 
+* Copyright (c) 2003-2006, Martín Pérez Mariñán, and individual 
+* contributors as indicated by the @authors tag. See copyright.txt in the
+* distribution for a full listing of individual contributors.
+* All rights reserved.
+* 
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the Modified BSD License as published by the Free 
+* Software Foundation.
+* 
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Modified
+* BSD License for more details.
+* 
+* You should have received a copy of the Modified BSD License along with 
+* this software; if not, write to the Free Software Foundation, Inc., 
+* 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the
+* FSF site: http://www.fsf.org.
+*/
+package org.jlibrary.web.servlet.listener;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.apache.log4j.Logger;
+import org.jlibrary.core.entities.Ticket;
+import org.jlibrary.core.factory.JLibraryServiceFactory;
+import org.jlibrary.core.profiles.LocalServerProfile;
+import org.jlibrary.core.security.SecurityException;
+import org.jlibrary.core.security.SecurityService;
+import org.jlibrary.web.services.StatsService;
+import org.jlibrary.web.services.TicketService;
+/**
+ * 
+ * @author dlatorre
+ *
+ */
+public class JLibrarySessionListener implements HttpSessionListener {
+	
+	private static Logger logger = Logger.getLogger(JLibrarySessionListener.class);
+	private StatsService statsService=StatsService.newInstance();
+	
+	public void sessionCreated(HttpSessionEvent hse) {
+		
+		hse.getSession().setMaxInactiveInterval(60);
+    	statsService.incTotalUsers();
+	}
+	
+	public void sessionDestroyed(HttpSessionEvent hse) {
+		
+    	statsService.decTotalUsers();
+    	
+    	Enumeration<String> attributeNames = hse.getSession().getAttributeNames();
+    	while (attributeNames.hasMoreElements()) {
+    		String name = attributeNames.nextElement();
+    		if (name.startsWith(TicketService.SESSION_TICKET_ID)) {
+    			Object value = hse.getSession().getAttribute(name);
+    			// Safety check
+    			if ((value != null) && (value instanceof Ticket)) {
+    				Ticket ticket = (Ticket)value;
+    				LocalServerProfile profile = new LocalServerProfile();
+    				SecurityService service = 
+    					JLibraryServiceFactory.getInstance(profile).getSecurityService();
+    				try {     					
+						service.disconnect((Ticket)ticket);
+	    				if (logger.isDebugEnabled()) {
+	    					logger.debug("User " + ticket.getUser() + " has been disconnected from jLibrary");
+	    				}
+					} catch (SecurityException e) {
+						logger.error("It was impossible to disconnect session for user " + ticket.getUser());
+					}
+    			}
+    		}
+    	}
+	}
+}
