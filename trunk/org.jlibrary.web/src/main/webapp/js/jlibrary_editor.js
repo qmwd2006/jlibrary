1ad9fb1b02bd82ed36699e1689ff060ef3a2e383
danilat <danilat@95db9afd-4a25-0410-8010-bfe18eb39bfe>
2008-01-20 01:24:47 +0000

    merge, categories and documents relations

--- trunk/org.jlibrary.web/src/main/webapp/js/jlibrary_editor.js
+++
@@ -1,7 +0,0 @@
-window.onload = makeDoubleDelegate(window.onload, WYSIWYGeditor);
-function WYSIWYGeditor(){
-	var oFCKeditor = new FCKeditor( 'content' ) ;
-	oFCKeditor.BasePath	= document.getElementById('baseUrl').value+'/FCKEditor/';
-	oFCKeditor.ToolbarSet = 'Basic';
-	oFCKeditor.ReplaceTextarea() ;
-}
